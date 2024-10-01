1a0e3b125ae3f252a3ab0e311879482ab94b7c42
Jonathan Metzler <jmetzler50@stuy.edu>
2024-10-01 14:50:56 -0400

    classwork 8/1

--- 13_combine/app.py
+++ 13_combine/app.py
@@ -7,7 +7,7 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 
-
+import csv, random
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -15,12 +15,32 @@ app = Flask(__name__)
 def hello_world():
     return "No hablo queso!"
 
+coll = []
+
 @app.route("/wdywtbwygp") 
 def test_tmplt():
-    return render_template('tablified.html', foo="fooooo", oogada= "combining elements of flask and html", boogada="Jonathan Metzler-Kyle Lee-Suhana Kumar - MLK")
+    #starts the table
+    page="<table><thead><tr><th>Occupations</th><th>Percentages</th></tr></thead><tbody>"
+    with open("occupations.csv", "r") as file:
+        next(file)
+        f = csv.reader(file)
+        dic = {}
+        for i in f:
+            dic.update({i[0]: float(i[1])})
+        total = dic["Total"]
+        key = dic.keys()
+        for k in key:
+            num = dic[k]
+            if (num / total) > random.random(): #random.random generates a float in (0, 1)
+                thing = k
+            else:
+                total -= num #Used for weighted probability; subtracts the probability from the total if the occupation is not chosen
+    for stuff in dic.keys():
+        page = page + stuff + "<br>"
+    page += "</tbody></table>"
+    return render_template('tablified.html', foo="fooooo", oogada= "combining elements of flask and html", boogada="Jonathan Metzler-Kyle Lee-Suhana Kumar - MLK", collection=coll)
 
 
 if __name__ == "__main__":
     app.debug = True
     app.run()
-
