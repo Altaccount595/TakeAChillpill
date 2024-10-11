1b8aacc1863d01ff7e73225a63fa777067632bee
Jonathan Metzler <jmetzler50@stuy.edu>
2024-10-11 11:46:25 -0400

    REMOVED ALL THE SPACES MUAHAHAHHA

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -4,7 +4,7 @@ from flask import request           #facilitate form submission
 
 
 app = Flask(__name__)    #create Flask object
-user = {"lol123":"pass123","noobslayer69":"gigachad","xxx_ultimategamer_xxx":"imadethispasswordswheniwas5"}
+user = {"lol123":["pass123", False],"noobslayer69":["gigachad", False],"xxx_ultimategamer_xxx":["imadethispasswordswheniwas5", False]}
 
 @app.route("/", methods=['GET', 'POST'])
 def disp_loginpage():
@@ -19,7 +19,7 @@ def disp_loginpage():
 #     print(request.args['username'])
     print("***DIAG: request.headers ***")
     print(request.headers)
-    return render_template( 'login.html' )
+    return render_template('login.html')
 
 
 @app.route("/auth", methods=['GET', 'POST'])
@@ -35,19 +35,20 @@ def authenticate():
 #     print(request.args['username'])
     print("***DIAG: request.headers ***")
     print(request.headers)
-    method1 = request.method
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        requesttype = "POST"
+    method1=request.method
+    if request.method=='POST':
+        username=request.form['username']
+        password=request.form['password']
+        requestType="POST"
     else:
-        username = request.args.get('username')
-        password = request.args.get('password')
-        requesttype = "GET"
-    if user[username]==password: 
-        return render_template( 'response.html', username=username, request = requesttype )  #response to a form submission
+        username=request.args.get('username')
+        password=request.args.get('password')
+        requestType="GET"
+    if user[username][0]==password:
+        user[username][1]=True
+        return render_template('response.html', username=username, request=requestType)  #response to a form submission
     else:
-        return render_template( 'login.html' )
+        return render_template('login.html')
 @app.route("/logout", methods=['GET', 'POST'])
 def logout():
     print("\n\n\n")
@@ -62,10 +63,11 @@ def logout():
     print("***DIAG: request.headers ***")
     print(request.headers)
     #method1 = request.method
-    
-    return render_template( 'logout.html')  #response to a form submission
+    for item in user.values():
+        item[1]=False
+    return render_template('logout.html')  #response to a form submission
     
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
-    app.debug = True 
+    app.debug=True 
     app.run()
